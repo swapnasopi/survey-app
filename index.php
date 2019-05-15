70ed56b478ce1df5db26d6bcc193c98836f264cb
swapnasopi <36980557+swapnasopi@users.noreply.github.com>
2019-05-15 15:31:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html ng-app="surveyapp">
+<head>
+  <meta charset="utf-8" />
+  <title>Survey App</title>
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.8/angular.min.js"></script>
+  <script src="app.js"></script>
+</head>
+
+<body>
+  <div class="container">
+      <div class="jumbotron text-center">
+    <h1 class="title">SurveyApp</h1>
+    <survey/>
+    </div>
+  </div>
+</body>
+
+</html>
